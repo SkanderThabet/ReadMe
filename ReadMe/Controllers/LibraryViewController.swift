393e732067db8d09c7f6fff3829dd956eeb3b6ec
SkanderThabet <skander.thabet@esprit.tn>
2022-03-22 01:52:13 +0100

    Added another custom cell

--- ReadMe/Controllers/LibraryViewController.swift
+++ ReadMe/Controllers/LibraryViewController.swift
@@ -15,7 +15,7 @@ class LibraryViewController: UITableViewController {
         else {
             fatalError("Nothing Selected!")
         }
-        let book = Library.books[indexPath.row]
+        let book = Library.books[indexPath.row - 1]
         return DetailViewController(coder: coder, book: book)
     }
     
@@ -33,14 +33,19 @@ class LibraryViewController: UITableViewController {
     //Mark:- DataSource
     //The number of things that go in this section
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        Library.books.count
+        Library.books.count + 1
     }
     //This method is to setup the cells
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath == IndexPath(row: 0, section: 0) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "NewBookCell", for: indexPath)
+            return cell
+        }
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(BookCell.self)", for: indexPath) as? BookCell
         else { fatalError("Could not create BookCell") }
         
-        let book = Library.books[indexPath.row]
+        let book = Library.books[indexPath.row - 1]
         cell.titleLabel.text=book.title
         cell.authorLabel?.text=book.author
         cell.bookThumbnail?.image=book.image
